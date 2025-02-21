db64974f046a2f09e75fc785b8420f4be6bb504b
Bernardo Coelho <bernardomcoelho@ua.pt>
2025-02-21 23:03:22 +0000

    Add files via upload

---
+++ P/src/aula02/Ex1.java
@@ -0,0 +1,23 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Segundos? ");
+        int number = sc.nextInt();
+        
+        int seconds = (number % 60);
+        int minutes = (number / 60) % 60 ;
+        int hours = (number / 3600) ;
+        
+        System.out.println("O tempo em segundos de " + number + " corresponde a " + String.format("%02d", hours) + ":"
+        + String.format("%02d", minutes) + ":"
+        + String.format("%02d", seconds)
+        );
+        
+        sc.close();
+    }
+}
+
---
+++ P/src/aula02/Ex4.java
@@ -0,0 +1,41 @@
+package aula02;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> lista = new ArrayList<Double>();
+
+        double numero;
+
+        System.out.print("Insere números: ");
+        numero = sc.nextDouble();
+        lista.add(numero);
+
+        double primeiroNumero = numero;
+
+        do {
+            System.out.print("Insere números: ");
+            numero = sc.nextDouble(); 
+            lista.add(numero);
+        } while (numero != primeiroNumero);
+
+
+        System.out.println(lista);
+        System.out.println("O valor máximo da lista é " + Collections.max(lista));
+        System.out.println("O valor mínimo da lista é " + Collections.min(lista));
+
+        int sum = 0;
+        for (double num : lista) {
+            sum += num;
+        }
+
+        System.out.println("A soma dos elementos da lista é "+sum);
+        System.out.println("o tamanho da lista é "+lista.size());
+
+
+        sc.close();
+    }
+}

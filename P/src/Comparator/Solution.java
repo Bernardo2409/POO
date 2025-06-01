26d82f0b3f3abf05f2846daf77a9f95bfc0904a7
Bernardo Coelho <bernardomcoelho@ua.pt>
2025-06-01 10:35:24 +0100

    .

--- P/src/Comparator/Solution.java
+++ P/src/Comparator/Solution.java
@@ -67,5 +67,22 @@ public class Solution
 }
 
 
+//Input
+
+// ID Name CGPA
+// 5
+// 33 Rumpa 3.68
+// 85 Ashis 3.85
+// 56 Samiha 3.75
+// 19 Samara 3.75
+// 22 Fahim 3.76
+ 
+// Output
+// Ashis
+// Fahim
+// Samara
+// Samiha
+// Rumpa
+
 
 
